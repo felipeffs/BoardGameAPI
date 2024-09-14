be1a85684dea50f0e00d52cf5f1a78f1e0f03d69
Felipe <felipe8ffs@gmail.com>
2024-09-14 07:34:08 -0300

    Creates project

---
+++ Data/BoardGame.cs
@@ -0,0 +1,12 @@
+﻿namespace BoardGameAPI.Data
+{
+    public class BoardGame
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; } = String.Empty;
+        public string Publisher { get; set; } = String.Empty ;
+        public string CoreMechanic { get; set; } = String.Empty;
+        public int MinPlayerCount { get; set; } = 1;
+        public int MaxPlayerCount { get; set;} = 1;
+    }
+}
